7d47a677d7a61e415ac995ba647b9de07eab8acb
VadimT <dflbv.tor@gmail.com>
2025-01-23 23:05:23 +0900

    created config using java

---
+++ src/main/Java/jp/java/springmvcpr/config/MySpringMvcDispatcherSerlvetInitialiser.java
@@ -0,0 +1,21 @@
+package jp.java.springmvcpr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherSerlvetInitialiser extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
