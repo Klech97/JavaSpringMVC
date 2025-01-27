6a8b7faba830c4ade0df8cec725af6fefe23bd4e
VadimT <dflbv.tor@gmail.com>
2025-01-27 23:18:52 +0900

    use variables from html

--- src/main/java/jp/java/springmvcpr/config/controllers/FirstController.java
+++ src/main/java/jp/java/springmvcpr/config/controllers/FirstController.java
@@ -3,11 +3,15 @@ package jp.java.springmvcpr.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        System.out.println("Hello " + name);
         return "first/hello";
     }
 
